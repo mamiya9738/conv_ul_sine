45c818d73e6c71e292c871ffdb2679149659ddd5
mamiya9738 <gogo9738@gmail.com>
2022-07-11 00:12:16 +0900

    表示名変更

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TextBox msg="ウルトラサイン変換ツール"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TextBox from './components/TextBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TextBox
   }
 }
 </script>
---
+++ src/components/TextBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <textarea v-model="message" placeholder=""></textarea>
+        <br>
+    <p style="white-space: pre-line;">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextBox',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
