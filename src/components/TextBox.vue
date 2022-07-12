97fafbd7da464fd64c32ee1ad3c421665ae52050
mamiya9738 <gogo9738@gmail.com>
2022-07-12 23:09:56 +0900

    中間保存(動かない)

--- src/components/TextBox.vue
+++ src/components/TextBox.vue
@@ -1,20 +1,34 @@
 <template>
-  <div class="hello">
+  <div class="TextBox" >
     <h1>{{ msg }}</h1>
     <textarea v-model="message" placeholder=""></textarea>
         <br>
     <p style="white-space: pre-line;">{{ message }}</p>
+
   </div>
+
+<div id="app">
+    <input type="text" v-model="textInput">
+    <p>{{ textInput }}</p>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'TextBox',
-  props: {
-    msg: String , 
-//    message: String , 
+    name: 'TextBox',
+    props: {
+      msg: String , 
+  //    message: String , 
+    }  
   }
-}
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      textInput: '初期値はここに入れておく',		
+    }
+  })	
+
 
 </script>
 
