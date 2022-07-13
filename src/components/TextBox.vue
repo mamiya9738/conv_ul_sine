63dbdc2c2d2edf286e46d1fbe296100f9888122f
mamiya9738 <gogo9738@gmail.com>
2022-07-13 23:34:01 +0900

    見直し(動かず)

--- src/components/TextBox.vue
+++ src/components/TextBox.vue
@@ -1,36 +1,11 @@
 <template>
-  <div class="TextBox" >
-    <h1>{{ msg }}</h1>
-    <textarea v-model="message" placeholder=""></textarea>
-        <br>
-    <p style="white-space: pre-line;">{{ message }}</p>
-
+  <div id="app">
+      <input type="text" v-model="textInput">
+      <p>{{ textInput }}</p>
   </div>
 
-<div id="app">
-    <input type="text" v-model="textInput">
-    <p>{{ textInput }}</p>
-</div>
 </template>
 
-<script>
-export default {
-    name: 'TextBox',
-    props: {
-      msg: String , 
-  //    message: String , 
-    }  
-  }
-
-var app = new Vue({
-    el: '#app',
-    data: {
-      textInput: '初期値はここに入れておく',		
-    }
-  })	
-
-
-</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
